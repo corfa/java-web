d5a8a7e952e2538a4cb4a04acbafd9985c7190c2
ivan <ivan.worker09@gmail.com>
2022-10-02 20:39:10 +0500

    Bootstrap

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -3,15 +3,20 @@ package com.example.demo.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class Post{
     private String text;
+
+    private Date creationDate;
     private Integer likes;
 
 
-    public Post(String text_post,Integer likes_post) {
+    public Post(String text_post,Integer likes_post,Date CreateDate) {
         text=text_post;
         likes=likes_post;
+        creationDate=CreateDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -4,15 +4,16 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts(){
         List<Post> list_post = new ArrayList<>();
-        list_post.add(new Post("Челябинск",555));
-        list_post.add(new Post("Москва",67));
-        list_post.add(new Post("Алания",1231));
+        list_post.add(new Post("Челябинск",555,new Date()));
+        list_post.add(new Post("Москва",67,new Date()));
+        list_post.add(new Post("Алания",1231,new Date()));
         return list_post;
     }
 }
